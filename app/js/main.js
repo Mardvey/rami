81096b19ef4ae1c41d0e07005fdf44116d59e9c3
Mardvey <stefan.irk96@mail.ru>
2020-03-12 08:55:00 +0800

    added section slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
 
-  
+  $('.slider__inner').slick({
+    prevArrow:'<button class="slick-arrow slick-prev"><div class="hunter"><img src="images/icons/slider-left.png" alt=""></div></button>',
+    nextArrow:'<button class="slick-arrow slick-next"><div class="hunter"><img src="images/icons/slider-right.png" alt=""></div></button>',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 3500,
+  });
 });
