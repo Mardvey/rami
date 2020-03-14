75580311f17293b36cbe44adce223582978e14c8
Mardvey <stefan.irk96@mail.ru>
2020-03-14 16:27:29 +0800

    added section project

--- app/js/main.js
+++ app/js/main.js
@@ -9,4 +9,16 @@ $(function(){
     autoplay: true,
     autoplaySpeed: 3500,
   });
+
+  $('.project__inner').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><div class="hunter"><img src="images/icons/slider-left.png" alt=""></div></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><div class="hunter"><img src="images/icons/slider-right.png" alt=""></div></button>',
+    dots: false,
+    infinite: false,
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
 });
